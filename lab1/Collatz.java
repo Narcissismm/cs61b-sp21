f6eeac5a8c4577ff529f9f39cd58e7bc6950a808
YangHaoWen <narcissismm@sjtu.edu.cn>
2026-03-05 00:05:09 +0800

    Fix pom.xml dependency spacing and duplicate errors

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,13 +3,13 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Fixed implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2==0) {
-            return n/2;
-        } else  (n %2==1) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else { // 修正1：else不能带条件，直接用else即可（或改为else if (n % 2 == 1)）
             return 3 * n + 1;
-
+        } // 修正2：补充缺失的闭合大括号
     }
 
     public static void main(String[] args) {
